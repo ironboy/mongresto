9a07300d72d353bfd8a0507a2580c684cd1e93b0
ironboy <thomas@clientsmile.se>
2015-06-15 05:53:54 +0200

    Cleaner ng code + important fixes
    Signed-off-by: ironboy <thomas@clientsmile.se>

--- mongresto.js
+++ mongresto.js
@@ -1,5 +1,5 @@
 /*
-  mongresto 0.17
+  mongresto 0.18
 
   June 2015 Nodebite AB, Thomas Frank
 
@@ -9,27 +9,69 @@
   working with mongoose models
   + generating Angular resource objects on the fly
 */
-var mongresto = {
 
-  init: function(app,dbName,apiPath,modelPath,ngResourcesPath){
 
-    // Defaults
-    console.log('Mongresto: Initializing!');
-    this.apiPath = apiPath || "/api";
-    this.modelPath = modelPath || "./mongoose-models/";
-    this.ngResourcesPath = ngResourcesPath || "/api/ngresources";
-    this.dbName = dbName || "test";
+/*
+  Nodebite code style -> jshint settings below, also 
+  indent = 2 spaces, keep your rows reasonably short
+  also your methods below sceen height.
+*/
+/* jshint 
+  loopfunc: true,
+  trailing: true,
+  sub: true,
+  expr: true,
+  noarg: false,
+  forin: false
+*/
 
-    // Connect to mongoose
-    this.mongoose = require("mongoose");
-    this.mongoose.connect('mongodb://localhost/' + this.dbName);
+
+var mongresto = module.exports = (function _mongresto(){ return {
+
+  defaults: {
+ 
+    // The MongoDB database to connect to
+    dbName: "test",
+
+    // The path to the rest api
+    apiPath: "/api",
     
-    // Check connection
-    this.mongoose.connection.once('open', function() {
-      console.log("Mongresto: Connected to database!");
-    });
+    // The path where you should put your Mongoose models
+    modelPath: "./mongoose-models/",
+    
+    // The path where Mongresto will autogenerate
+    // frontend JavaScript containing ngResource-based objects
+    ngResourcesPath: "/api/ngresources",
+    
+    // If Angular.js should stop all further requests to the backend
+    // if one result comes back as an error
+    ngStopQueueOnError: false,
+    
+    // A function written by you - it gets access to the current question
+    // and can deny Mongresto permission to run it
+    
+    permissionToAsk:
+      function(modelName, method, query, rbody){ return true; },
+    
+    // A function written by you - it gets access to the current result
+    // (and question) and can deny Mongresto permission to return it
+    permissionToAnswer:
+      function(modelName, method, query, rbody, result){ return true; } 
+      
+  },
 
-    // Load mongoose models
+  init: function(app,options){
+
+    console.log("Mongresto: Initializing!");
+
+    // Use defaults if no option is set
+    options = options || {};
+    for(var i in this.defaults){
+      this[i] = options[i] || this.defaults[i];
+    }
+
+    // Connect to DB and load mongoose models
+    this.connectToDb();
     this.loadMongooseModels();
 
     // A url path that will respond with ngresources
@@ -44,8 +86,37 @@ var mongresto = {
 
   },
 
+  connectToDb: function(){
+    // Connect to mongoose
+    var dbName = this.dbName;
+    this.mongoose = require("mongoose");
+    this.mongoose.connect('mongodb://localhost/' + dbName);
+    
+    // Check connection
+    this.mongoose.connection.once('open', function() {
+      console.log("Mongresto: Connected to database " + dbName + "!");
+    });
+  },
+
   apiCall: function(req,res){
 
+    // Important! Since Node.js is single-threaded
+    // saving things in the this scope
+    // and depending on them to be there 
+    // before and after async db calls
+    // is a recipe for disaster / mix-ups
+    // of different questions (and users)
+
+    // To avoid this we clone a new instance of the 
+    // main object/the library and run from there.
+    // (This is a nice alternative to sending around 
+    //  a lot of parameters between methods - old school node)
+
+    Object.create(this).apiCallHandler(req,res);
+
+  },
+
+  apiCallHandler: function(req,res){
     // Save the req and res objects + http method
     this.req = req;
     this.res = res;
@@ -68,7 +139,6 @@ var mongresto = {
 
     // Run query
     this.runQuery();
-
   },
 
   getFileNames: function(path,callback){
@@ -192,50 +262,72 @@ var mongresto = {
     this.search = {$or:or};
   },
 
+  checkPermission: function(type,result){
+    // Check if we have permission to ask or answer
+    // a question, usig user defined functions from init options
+    var method = this["permissionTo"+type];
+    if(typeof method != "function"){return true;}
+    if(method(
+      this.model.modelName,
+      this.method,
+      this.search,
+      this.req.body,
+      result
+    )){return true;}
+    this.responder("Forbidden",false,403);
+  },
+
   runQuery: function(){
+
+    // Check permissions
+    if(!this.checkPermission("Ask")){return;}
+
+    // Make responder remember "this"
+    // - important since this is not the original mongresto object
+    // but rather a clone of it (look in the apiCall method)
+    var that = this, responder = function(){that.responder.apply(that,arguments);};
+
     if(this.method == "GET"){
       if(!this.populate){
-        this.model.find(this.search,this.responder);
+        this.model.find(this.search,responder);
       }
       else {
-        this.model.find(this.search).populate(this.populate).exec(this.responder);
+        this.model.find(this.search).populate(this.populate).exec(responder);
       }
     }
+
     if(this.method == "DELETE"){
-      this.model.find(this.search).remove(this.responder);
+      this.model.find(this.search).remove(responder);
     }
+
     if(this.method == "PUT"){
-      this.model.update(this.search,this.req.body,{multi:true},this.responder);
+      this.model.update(this.search,this.req.body,{multi:true},responder);
     }
+
     if(this.method == "POST"){
-      // A bit more complex than the other calls since we want to allow
-      // multiple items to be created at once
-      var b = this.req.body, that = this;
+      var b = this.req.body;
       b = b.push ? b : [b];
       var l = b.length, r = [], anyError = false;
-      for(var i = 0; i < l; i++){
-        new this.model(b[i]).save(f);
-      }
-      function f(err,result){
+      var f = function(err,result){
         if(anyError){return;}
-        if(err){that.responder(err,false);anyError = true;return;}
+        if(err){responder(err,false);anyError = true;return;}
         r.push(result);
         l--;
-        if(l===0){that.responder(false,r);}
+        if(l===0){responder(false,r);}
+      };
+      for(var i = 0; i < l; i++){
+        new this.model(b[i]).save(f);
       }
     }
   },
 
-  responder : function(error,result){
+  responder: function(error,result,errorCode){
     // Respond to CRUD calls
-    var that = mongresto;
-    var singleton = !that.forceArray && that.req.method == "GET" && that.search._id;
-    that.res.status(error ? 500 : 200);
-    that.res.json(
-      (error && {_error:error}) ||
-      (singleton && result[0]) ||
-      (!singleton && result)
-    );
+    var singleton = !this.forceArray && this.req.method == "GET" && this.search._id;
+    this.res.status(error ? errorCode || 500 : 200);
+    result = (singleton && result[0]) || (!singleton && result);
+    if(!this.checkPermission("Answer",result)){return;}
+    this.res.json((error && {_error:error}) || result);
   },
 
   buildNgResourcesScript: function(req,res){
@@ -244,153 +336,189 @@ var mongresto = {
     var ngAppVarName = req.url.split("/").pop();
     var models = this.mongooseModels;
     var modelNames = this.mongooseModelNames;
-    var content = (this.ngResourcesScriptTemplate+"")
-      .replace(/\n\s{4,4}/g,'\n')
+    var content = "(function(){var mongresto=" +
+      ("{\nngInit:" + (_mongresto+"").split("ngInit:")[3])
       .replace(/\[111\]/,JSON.stringify(modelNames))
+      .replace(/\[222\]/,this.ngStopQueueOnError)
       .replace(/pathToApi/,this.apiPath)
       .replace(/ngAppName/,ngAppVarName)
-      .split("\n");
-    content.shift(); content.pop(); content = content.join("\n");
+      .split(', // end ng methods')[0] +
+      "\n};\nmongresto.ngInit();})()";
     res.header("Content-type","application/javascript");
     res.send(content);
   },
 
-  // Template used by buildNgResourcesScript
-  // this is a long intricate method (never called on the backend)
-  // unfortunately it is hard to refactor into small clean methods
-  // but it it the tool we need to write superclean ng controller code
-  ngResourcesScriptTemplate: function(){
-    // Autogenerate ngResources
-    // and wrap in a queue system
-    // that make them "synchronous"
-    // i.e. executes method calls in order
-    (function(){
-      // Create the following ngResources
-      var models = [111];
-      // Extend RegExp for json serialization
-      RegExp.prototype.toJSON = function(){
-        return "~regexpstart~"+this+"~regexpend~";
-      };
-      // Handle relations
-      function handleRelate(r){
-        if(r.methodName == "update" && r.orgArgs[0] && r.orgArgs[0]._relate){
-          var u = r.orgArgs[0]._relate;
-          var obj = {};
-          for(var i in u){
-            obj[i] = obj[i] || [];
-            u[i] = u[i].push ? u[i] : [u[i]];
-            for(var j = 0; j < u[i].length; j++){
-              u[i][j]._id && obj[i].push(u[i][j]._id);
-            }
-          }
-          if(obj.items){
-            obj.__idsToLookFor__ = obj.items;
-            delete obj.items;
-            r.args[0] = {id: '{_relate:true}'};
-            r.args.splice(1,0, obj);
-          }
+  ngInit: function(){
+    // Extend RegExp for json serialization
+    RegExp.prototype.toJSON = function(){
+      return "~regexpstart~"+this+"~regexpend~";
+    };
+
+    // Important global props
+    this.stopQueueOnError = [222];
+    this.queue = [];
+    this.busy = false;
+    this.callOnNoQueue = [];
+
+    // Create resources
+    var that = this;
+    [111].forEach(function(x){that.ngCreateResource(x);});
+  },
+
+  ngMethods: function(){
+    // Define ngResource methods
+    return {
+      get: { method: 'GET', isArray: true },
+      getById: { method: 'GET', isArray: false },
+      create: { method: 'POST', isArray: true},
+      update: { method: 'PUT' },
+      remove: { method: 'DELETE' }
+    };
+  },
+
+  ngCreateResource: function(entity){
+    var that = this, methods = this.ngMethods();
+    ngAppName.factory(entity, ["$resource", function ($resource) {
+
+      // Create an ng resource
+      var re = $resource(
+        "pathToApi/" + entity.toLowerCase() + "/:id/",
+        {id:"@id"}, methods
+      );
+      
+      // Wrap it in our own special object (queue handling etc)
+      var obj = {
+        onQueueDone: function(func){
+          typeof func == "function" && that.callOnNoQueue.push(func);
         }
-         
-      }
-      // Queue and queue handler engine
-      var queue = [], busy = false, callOnNoQueue = [];
-      function runQueue(){
-        busy = true;
-        var r = queue.shift();
-        var orgCallback = typeof r.args[r.args.length-1] == "function" && r.args.pop();
-        r.args.push(function(){
-          var i;
-          if(r.result.push){
-            for(i = 0; i < result.length; i++){r.result.push(result[i]);}
-          }
-          else {
-            for(i in result){r.result[i] = result[i];}
-          }
-          var oldLength = queue.length;
-          orgCallback && orgCallback.apply(r.re,arguments);
-          // Reorganize queue for more syncronous feel
-          // if we are called in a callback
-          var moveFirst = [];
-          while(queue.length > oldLength){
-            moveFirst.push(queue.pop());
-          }
-          while(moveFirst.length){
-            queue.unshift(moveFirst.shift());
-          }
-          busy = false;
-          queue.length && runQueue();
-          if(!queue.length && !busy){
-            while(callOnNoQueue.length){callOnNoQueue.shift()();}
-          }
-        });
-        handleRelate(r);
-        var result = r.re[r.methodName].apply(r.re,r.args);
-      }
-      // Loop through model names
-      models.forEach(function(entity){
-        // Create an ng resource
-        ngAppName.factory(entity, ["$resource", "$timeout", function ($resource) {
-          var methods = {
-            get: { method: 'GET', isArray: true },
-            getById: { method: 'GET', isArray: false },
-            create: { method: 'POST', isArray: true},
-            update: { method: 'PUT' },
-            remove: { method: 'DELETE' }
-          };
-          var re = $resource(
-            "pathToApi/" + entity.toLowerCase() + "/:id/",
-            {id:"@id"}, methods
-          );
-          // Wrap it in our own queue handler
-          var obj = {};
-          for(var i in methods){
-            (function(){
-              var methodName = i, methodSpec = methods[i];
-              obj[i] = function(){
-                var args = Array.prototype.slice.call(arguments);
-                var orgArgs = [].concat(args);
-                if(methodName == "create"){args.unshift({});}
-                if(typeof args[0] == "string"){
-                  args[0] = {_id:args[0]};
-                }
-                var onlyId = true;
-                for(var i in args[0]){
-                  if(i == "_id" || i == "_populate"){continue;}
-                  onlyId = false;
-                }
-                if(args[0] && onlyId && methodSpec.isArray){args[0].__forceArray = true;}
-                if(typeof args[0] == "object" && !args[0].id){
-                  args[0] = {
-                    id:JSON.stringify(args[0])
-                    .replace(/"~regexpstart~/g,'')
-                    .replace(/~regexpend~"/g,'')
-                  };
-                }
-                var result = methodSpec.isArray ? [] : {};
-                queue.push({
-                  re:re,
-                  orgArgs: orgArgs,
-                  args:args,
-                  methodName:methodName,
-                  methodSpec:methodSpec,
-                  result:result
-                });
-                if(!busy){runQueue();}
-                return result;
-              };
-            })();
-          }
-          obj.onQueueDone = function(func){
-            if(typeof func != "function"){return;}
-            callOnNoQueue.push(func);
-          }
-          return obj;
-        }]);
+      };
+      for(var i in methods){that.ngCreateMethod(re,obj,methods,i);}
+      
+      return obj;
+    }]);
+  },
+
+  ngCreateMethod: function(re,obj,methods,methodName){
+
+    var that = this, methodSpec = methods[methodName];
+
+    // Create a method
+    obj[methodName] = function(){
+
+      // On a call to the method preprocess the arguments
+      args = that.ngPreprocessArguments(arguments,methodName, methodSpec);
+      var orgArgs = args[0];
+      args = args[1];
+
+      // Create an empty result to fill later
+      var result = methodSpec.isArray ? [] : {};
+
+      // Push to queue
+      that.queue.push({
+        re:re, orgArgs: orgArgs, args:args,
+        methodName:methodName, methodSpec:methodSpec,
+        result:result
       });
-    })();
-  }
+      // Run queue if not busy
+      if(!that.busy){that.ngRunQueue();}
 
-};
+      // Return the result
+      return result;
+    };
 
-// Export the mongresto object as a module
-module.exports = mongresto;
+  },
+
+  ngPreprocessArguments: function(args, methodName, methodSpec){
+    // Preprocesses arguments to different methods
+    args = Array.prototype.slice.call(args);
+    var orgArgs = [].concat(args);
+    // One argument less than ng standard for create
+    if(methodName == "create"){args.unshift({});}
+    // Strings are ids
+    if(typeof args[0] == "string"){args[0] = {_id:args[0]};}
+    // Ask backend to sometimes force arrays to avoid ng errors
+    var onlyId = true;
+    for(var i in args[0]){
+      if(i == "_id" || i == "_populate"){continue;}
+      onlyId = false;
+    }
+    if(args[0] && onlyId && methodSpec.isArray){
+      args[0].__forceArray = true;
+    }
+    
+    // Stringify search object
+    if(typeof args[0] == "object" && !args[0].id){
+      args[0] = {
+        id:JSON.stringify(args[0])
+        .replace(/"~regexpstart~/g,'')
+        .replace(/~regexpend~"/g,'')
+      };
+    }
+
+    return [orgArgs, args];
+  },
+
+  ngRunQueue: function(){
+    // Read from queue
+    this.busy = true;
+    var r = this.queue.shift(), that = this;
+    // Add a callback function to run when the results are back
+    var orgCallback = typeof r.args[r.args.length-1] == "function" && r.args.pop();
+    var newCallback = function(){
+      var i, isArr = r.result.push;
+      // Transfer result to waiting array or object
+      if(isArr){for(i = 0; i < result.length; i++){r.result.push(result[i]);}}
+      else {for(i in result){r.result[i] = result[i];}}
+      var oldLength = that.queue.length;
+      orgCallback && orgCallback.apply(r.re,arguments);
+      // Reorganize queue for more syncronous feel
+      // if we are called in a callback
+      var moveFirst = [];
+      while(that.queue.length > oldLength){moveFirst.push(that.queue.pop());}
+      while(moveFirst.length){that.queue.unshift(moveFirst.shift());}
+      // We are done - run next item in queue?
+      that.busy = false;
+      that.queue.length && that.ngRunQueue();
+      // The queue is empty and the last result is back
+      if(!that.queue.length && !that.busy){
+        while(that.callOnNoQueue.length){that.callOnNoQueue.shift()();}
+      }
+    };
+    if(this.stopQueueOnError){r.args.push(newCallback);}
+    else {r.args.push(newCallback, newCallback);}
+    // Call ngResource object
+    this.ngHandleRelate(r);
+    var result = r.re[r.methodName].apply(r.re,r.args);
+  },
+
+  ngHandleRelate: function(r){
+    // Check if we need to handle update _relate actions
+    if(
+      r.methodName != "update" ||
+      !r.orgArgs[0] ||
+      !r.orgArgs[0]._relate
+    ){
+      return;
+    }
+    
+    // Rebuild the search object from the _relate object
+    var u = r.orgArgs[0]._relate;
+    var obj = {};
+    for(var i in u){
+      obj[i] = obj[i] || [];
+      u[i] = u[i].push ? u[i] : [u[i]];
+      for(var j = 0; j < u[i].length; j++){
+        u[i][j]._id && obj[i].push(u[i][j]._id);
+      }
+    }
+    
+    // Add ids to look for to request body
+    if(obj.items){
+      obj.__idsToLookFor__ = obj.items;
+      delete obj.items;
+      r.args[0] = {id: '{_relate:true}'};
+      r.args.splice(1,0, obj);
+    }
+  }, // end ng methods
+
+};})();
